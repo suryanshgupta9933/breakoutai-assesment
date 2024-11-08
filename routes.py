133a273e2dbc1a7f2058c1b09316350c23cbf156
Suryansh Gupta <suryanshgupta9933@gmail.com>
2024-11-09 01:57:31 +0530

    added root

--- routes.py
+++ routes.py
@@ -1,36 +1,17 @@
 # Importing Dependencies
-import pandas as pd
-from io import BytesIO
-from pydantic import BaseModel
-from fastapi import FastAPI, UploadFile, Form
-from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI
+
+from API.upload import router as upload_router
 
 # FastAPI app
 app = FastAPI()
 
-# CORS for Streamlit communication
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+# Including Routers
+app.include_router(upload_router)
 
-# Pydantic model for FileColumn
-class FileColumn(BaseModel):
-    column_name: str
-
-@app.post("/upload-csv/")
-async def upload_csv(file: UploadFile):
-    # Read CSV file and return columns
-    df = pd.read_csv(BytesIO(await file.read()))
-    return {"columns": df.columns.tolist()}
-
-@app.post("/process-query/")
-async def process_query(column_data: FileColumn, query: str):
-    # Dummy response
-    return {"message": f"Processing query '{query}' for column '{column_data.column_name}'"}
+@app.get("/")
+async def root():
+    return {"message": "Dashboard API works!"}
 
 if __name__ == "__main__":
     import uvicorn

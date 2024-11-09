e9127b17b89175d63d1149bc399c41b8ebf1ab9d
Suryansh Gupta <suryanshgupta9933@gmail.com>
2024-11-09 21:40:52 +0530

    added pipeline route

--- routes.py
+++ routes.py
@@ -1,18 +1,20 @@
 # Importing Dependencies
+import uvicorn
 from fastapi import FastAPI
 
 from API.upload import router as upload_router
+from API.pipeline import router as pipeline_router
 
 # FastAPI app
 app = FastAPI()
 
 # Including Routers
 app.include_router(upload_router)
+app.include_router(pipeline_router)
 
 @app.get("/")
 async def root():
     return {"message": "Dashboard API works!"}
 
 if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run("routes:app", host="0.0.0.0", port=8000)

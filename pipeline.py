7fe7fb4934bb65f71159de241612ece56e83e764
Suryansh Gupta <suryanshgupta9933@gmail.com>
2024-11-12 19:13:39 +0530

    added web scraper to pipeline

--- pipeline.py
+++ pipeline.py
@@ -4,9 +4,10 @@ import logging
 import asyncio
 import pandas as pd
 
-from src.process_query import process_query
+from src.search_query import preprocess
 from src.google_search_module import GoogleSearchModule
 from src.url_filter import remove_redundant_links
+from src.web_scraper import scrapper
 
 # Configure logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
@@ -19,14 +20,16 @@ async def run_pipeline(query: str, column_name: str, df: pd.DataFrame):
     """
     try:
         # Process the query
-        search_queries = process_query(query, column_name, df)
+        search_queries = preprocess(query, column_name, df)
         # Initialize Google Search Module
         search_module = GoogleSearchModule(search_queries)
         # Perform Google search
         url_results = await search_module.get_all_urls(search_queries)
         # Filter out redundant links
         filtered_results = remove_redundant_links(url_results)
-        return filtered_results
+        # Scrape the urls to extract text
+        scrapped_data = scrapper(filtered_results)
+        return scrapped_data
 
     except Exception as e:
         logger.exception("An unexpected error occurred in the pipeline.")

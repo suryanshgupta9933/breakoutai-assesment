0ab6208bafcee53b838c9361ed93227c5c6334b6
Suryansh Gupta <suryanshgupta9933@gmail.com>
2024-11-13 02:14:54 +0530

    refined prompt

--- src/prompt.py
+++ src/prompt.py
@@ -4,9 +4,6 @@ import langchain
 
 # System prompt for creating a field name
 def get_field_name_system_prompt():
-    """
-    System prompt for creating a field name.
-    """
     prompt = """You are an expert data assistant tasked with aiding in the creation and management of CSV files.
 Your primary function is to assist with defining the field names for the columns in the CSV file based on the user query.
 
@@ -15,7 +12,27 @@ You can use the following strategies to create field names:
 2. Keep the field names concise and descriptive.
 3. Ensure the field names are unique and distinguishable.
 4. Use snake_case for multi-word field names.
+5. Choose any relevant metric or unit to create a field name.
+6. Write any measurements in the International System of Units (SI) format.
+7. When in doubt, use 'Field_1', 'Field_2', etc., as generic field names.
+8. Your response must only contain the field name without any additional information.
 
-Based on the given user query, create a field name for the column in the CSV file.
-"""
+Based on the given user query, create a field name for the column in the CSV file."""
+    return prompt
+
+# System prompt for creating field data based on the field name, query and context
+def get_field_data_system_prompt():
+    prompt = """You are a data operations specialist equipped to process and format data for CSV integration. Your role requires high precision, ensuring that every piece of data aligns perfectly with pre-defined CSV structures and constraints.
+
+For each query:
+1. Identify the relevant data points that correspond to the specified field names.
+2. Convert all data into a CSV-compatible format, following these detailed instructions:
+    a. Numbers: Represent all numbers in a standardized numeric format (e.g., convert all currency to USD and use two decimal places).
+    b. Dates: Convert dates into ISO format (YYYY-MM-DD).
+    c. Text: Use plain text, strip out any emojis or special characters, and ensure consistency in capitalization as directed (e.g., 'Title Case', 'UPPER CASE', or 'lower case').
+    d. Handle missing or unclear data by populating the field with 'Null' or 'None' as appropriate.
+3. Write any metrics or measurements in the International System of Units (SI) format.
+4. Mention things like currency, units, and other measurements in the appropriate format.
+4. Ensure all data is correctly formatted and aligned with the specified field name.
+5. Your response must only contain the data for the specified field name, formatted according to the instructions."""
     return prompt
